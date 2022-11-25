b3e54f4bd808516ec95f6bad749560186aa044e9
addetheo <56866727+addetheo@users.noreply.github.com>
2022-11-25 21:43:00 +0200

    Implemented

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,8 @@
 package lang.print.gaps.task6;
-
+//You will be given a float number, and your task will be to find a correct way to round this variable to integer variable,
+// corresponding to Math rules (requires googling). Print that variable to console.
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        System.out.println(Math.round(numberToBeRounded));
     }
 }
--- src/main/java/lang/print/gaps/task6/OperatorPrecedence.java
+++ src/main/java/lang/print/gaps/task6/OperatorPrecedence.java
@@ -1,10 +1,11 @@
 package lang.print.gaps.task6;
-
+//Introduce changes (add brackets) to code snippets below to get expected results
+// (for System.out.println(firstBool && secondBool || thirdBool && fourthBool || (varSix = 5) < 0);only adding brackets is allowed):
 public class OperatorPrecedence {
     public static void main(String[] args) {
         int first = 1;
         int second = 2;
-        int eight = 3 + first * second;
+        int eight = (3 + first) * second;
         System.out.println(eight);//eight is expected to be 8
         boolean firstBool = false;
         boolean secondBool = false;
@@ -12,7 +13,7 @@ public class OperatorPrecedence {
         boolean fourthBool = 4 <= 101;
         int varSix = 17;
 
-        System.out.println(firstBool && secondBool || thirdBool && fourthBool || (varSix = 5) < 0);
+        System.out.println(firstBool && (secondBool || thirdBool) && fourthBool || (varSix = 5) < 0);
         System.out.println(varSix);
     }
 }
